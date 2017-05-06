98b62a7cc0987ea3e117df610fa433c967b29a9e
Kiruthik Kumar <mail@kiruthik.com>
2017-05-06 15:12:09 -0400

    a

--- createVMSwitch.ps1
+++ createVMSwitch.ps1
@@ -1,4 +1,5 @@
 Import-Module Hyper-V
 
+$ethernet = Get-NetAdapter -Name ethernet
 
 New-VMSwitch -Name iee    -NetAdapterName $ethernet.Name -AllowManagementOS $true -Notes ‘Intel Ethernet (External)’
