33a2ccc288c26640b645b55f5bbfc1e594735687
Kiruthik Kumar <mail@kiruthik.com>
2017-05-06 16:25:14 -0400

    create VM scripts

---
+++ createDOCOVM.ps1
@@ -0,0 +1,11 @@
+
+
+$VM = New-VM  -Name 'Win2012R2-DOCO-AD'  -MemoryStartUpBytes  3GB   -Path  'C:\ProgramData\Microsoft\Windows\Hyper-V'   -VHDPath  "D:\VM_Ds\HyperV_Disks\Win2012R2-DOCO-128GB\Win2012R2-AD-4mSSD.VHDX"   -ErrorAction  'Stop' -Verbose  $True
+
+
+# set processor thread
+Set-VMProcessor $VM.name   -Count 2  -Reserve 10 -Maximum 75 -RelativeWeight 200
+
+
+# set static mac address for the router to set the Static IP
+Add-VMNetworkAdapter  $vm   -StaticMacAddress 'D0:C0:82:01:D0:C0' -SwitchName 'IEE' 
--- createVM4SP2013-s1.ps1
+++ createVM4SP2013-s1.ps1
@@ -1,1 +1,4 @@
 $VM = New-VM  -Name 'SP2013-S1'  -MemoryStartUpBytes  6GB   -Path  'C:\ProgramData\Microsoft\Windows\Hyper-V'  -SwitchName  "IEE" -VHDPath  "O:\VHDX\2013_withDB\sp2013_withDB.vhdx"   -ErrorAction  'Stop' -Verbose  $True
+
+Set-VMProcessor sp2013-s1  -Count 4  -Reserve 10 -Maximum 75 -RelativeWeight 200
+
