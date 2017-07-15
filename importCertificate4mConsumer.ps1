afdb6db71687b3a39be42f6289c4a41c00ddee62
SP Admin <SPAdmin@doco.kiruthik.com>
2017-07-14 20:38:07 -0400

    variable name fixed

--- importCertificate4mConsumer.ps1
+++ importCertificate4mConsumer.ps1
@@ -1,7 +1,7 @@
 
 
 $trustRootCert = Get-PfxCertificate 'C:\kk\ConsumingFarmRoot.cer'
-New-SPTrustedRootAuthority 'SP2013-Root-ConsumingFarm' -Certificate $trustCert
+New-SPTrustedRootAuthority 'SP2013-Root-ConsumingFarm' -Certificate $trustRootCert
 
 
 $trustSTSCert = Get-PfxCertificate 'C:\kk\ConsumingFarmSTS.cer'
