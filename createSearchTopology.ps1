fb067983e1aa7b3485ec18eb8f795b635bbde972
KK <mail@kiruthik.com>
2017-11-10 15:03:52 -0500

    added scripts for 4 server config

--- createSearchTopology.ps1
+++ createSearchTopology.ps1
@@ -6,14 +6,45 @@ $servername = $env:computername
 $searchServiceApp = Get-SPEnterpriseSearchServiceApplication  -Identity "Dedicated SP2016 Search Service"
 $searchTopology = New-SPEnterpriseSearchTopology -SearchApplication $searchServiceApp 
 
-New-SPEnterpriseSearchAdminComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
-New-SPEnterpriseSearchCrawlComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
-New-SPEnterpriseSearchContentProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
-New-SPEnterpriseSearchAnalyticsProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
-New-SPEnterpriseSearchQueryProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
+#New-SPEnterpriseSearchAdminComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
+#New-SPEnterpriseSearchCrawlComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
+#New-SPEnterpriseSearchContentProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
+#New-SPEnterpriseSearchAnalyticsProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
+#New-SPEnterpriseSearchQueryProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  
 
 ## Mentioning the IndexPartition 
-New-SPEnterpriseSearchIndexComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  -IndexPartition 0
+#New-SPEnterpriseSearchIndexComponent -SearchTopology $searchTopology  -SearchServiceInstance $servername  -IndexPartition 0
 
 
-Set-SPEnterpriseSearchTopology -Identity  $searchTopology 
+#Set-SPEnterpriseSearchTopology -Identity  $searchTopology 
+
+
+ ## add components for Host1(VMSrch01)
+New-SPEnterpriseSearchAdminComponent -SearchTopology $searchTopology  -SearchServiceInstance $host1  
+New-SPEnterpriseSearchCrawlComponent -SearchTopology $searchTopology  -SearchServiceInstance $host1  
+New-SPEnterpriseSearchIndexComponent -SearchTopology $searchTopology  -SearchServiceInstance $host1   -IndexPartition 0
+
+ ## add components for Host2(VMSrch02)
+New-SPEnterpriseSearchQueryProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host2
+New-SPEnterpriseSearchAnalyticsProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host2   
+New-SPEnterpriseSearchContentProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host2
+
+
+ ## add components for Host3(VMSrch03)
+ New-SPEnterpriseSearchQueryProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host3
+ New-SPEnterpriseSearchContentProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host3
+ New-SPEnterpriseSearchAnalyticsProcessingComponent -SearchTopology $searchTopology  -SearchServiceInstance $host3   
+
+
+ ## add components for Host4(VMSrch04)
+ New-SPEnterpriseSearchIndexComponent -SearchTopology $searchTopology  -SearchServiceInstance $host4   -IndexPartition 0
+ New-SPEnterpriseSearchAdminComponent -SearchTopology $searchTopology  -SearchServiceInstance $host4  
+ New-SPEnterpriseSearchCrawlComponent -SearchTopology $searchTopology  -SearchServiceInstance $host4  
+
+#make this new search topology ACTIVE
+ Set-SPEnterpriseSearchTopology -Identity  $searchTopology 
+
+
+ ## check the topology / status
+ Get-SPEnterpriseSearchTopology -SearchApplication $searchServiceApp
+ Get-SPEnterpriseSearchStatus -SearchApplication $searchServiceApp -Text
